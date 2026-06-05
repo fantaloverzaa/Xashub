9ca8c1090e0ef4b3bbd8f0f921aee6bec974c31b
Fantalover <sunkistsugar2@gmail.com>
2026-06-05 22:54:18 +1000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/fantaloverzaa/ui/refs/heads/main/Key.lua"))()
-
-task.spawn(function()
-	while not game.CoreGui:FindFirstChild("key") do
-		
-wait(0.1)
-
 local Games = {
     [3398014311] = function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/fantaloverzaa/Xashub/refs/heads/main/game/rty2.lua"))()
